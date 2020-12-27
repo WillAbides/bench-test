268e44081bfecd2144a2b99abd075df0d0987c7f
Will Roden <willabides@github.com>
2020-12-27 13:36:30 -0600

    initial commit

---
+++ bench_test.go
@@ -0,0 +1,47 @@
+package testrepo
+
+import (
+	"io/ioutil"
+	"os"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func getSleepTime(filename string) (time.Duration, error) {
+	b, err := ioutil.ReadFile(filename)
+	if os.IsNotExist(err) {
+		return 10 * time.Millisecond, nil
+	}
+	if err != nil {
+		return 0, err
+	}
+	i, err := strconv.Atoi(string(b))
+	if err != nil {
+		return 0, err
+	}
+	return time.Duration(i) * time.Millisecond, nil
+}
+
+type sleeper struct {
+	sleepTime time.Duration
+}
+
+func (s *sleeper) sleep() {
+	time.Sleep(s.sleepTime)
+}
+
+func Benchmark_sleeper_sleep(b *testing.B) {
+	sleepTime, err := getSleepTime("sleeptime.txt")
+	if err != nil {
+		b.Fatal(err)
+	}
+	sl := &sleeper{
+		sleepTime: sleepTime,
+	}
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		sl.sleep()
+	}
+}
